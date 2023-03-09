55010820d5ab3c881a6ad051a3ce34ff790a367d
Renato Spakauskas <renatospaka@gmail.com>
2023-03-09 20:20:16 -0300

    feat: :necktie: server logic
    returning {bid: <value>} but not using context

--- client/client.go
+++ client/client.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"context"
-	"io"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -12,15 +13,39 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
 	defer cancel()
 
-	r, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080", nil)
+	_, err := getDolarRate(ctx)
 	if err != nil {
 		panic(err)
+	}
+
+}
+
+func getDolarRate(ctx context.Context) (exchangeRate float32, err error) {
+	exchangeRate = 0.0;
+	err = nil
+
+	r, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080", nil)
+	if err != nil {
+		return 0.0, err
 	}
 
 	rs, err := http.DefaultClient.Do(r)
 	if err != nil {
-		panic(err)
+		return 0.0, err
 	}
+	defer rs.Body.Close()
 
-	io.Copy(os.Stdout, rs.Body)
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		return
+	}
+	log.Println("Cotação:", body)
+
+	var cotacao interface{}
+	err = json.Unmarshal(body, &cotacao)
+	return 
+}
+
+func saveRateToText(exchangeRate float32) (err error) {
+	return err
 }
--- server/server.go
+++ server/server.go
@@ -1,29 +1,93 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
+	"strconv"
 )
 
-func main () {
-	http.HandleFunc("/", handler)
+type USDBRL struct {
+	USDBRL cotacao
+}
+type cotacao struct {
+	Code      string  `json:"code"`
+	Codein    string  `json:"code-in"`
+	Name      string  `json:"nae"`
+	High      string `json:"high-value"`
+	Low       string `json:"low-value"`
+	VarBid    string `json:"var-bid"`
+	PctChange string `json:"pct-change"`
+	BID       string `json:"bid"`
+	ASK       string `json:"ask"`
+	timestamp string  `json:"-"`
+	CreatedAt string  `json:"created-at"`
+}
+
+type BID struct {
+	BID 	float32 `json:"bid"`
+}
+
+func main() {
+	http.HandleFunc("/cotacao", getDolarRateHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
+func getDolarRateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/cotacao" {
+		http.Error(w, "route not found", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
 
-func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	log.Println("Request inicializado")
 	defer log.Println("Request finalizado")
 
-	select {
-	case <-time.After(5 * time.Second):
-		log.Println("Request executado com sucesso")
-		w.Write([]byte("Request executado com sucesso\n"))
-
-	case <-ctx.Done():
-		log.Println("Request cancelado pelo cliente")
-		// http.Error(w, "Request cancelado pelo cliente", http.StatusRequestTimeout)
+	cotacao, err := getDolarRate(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	
+	w.WriteHeader(http.StatusOK)
+	var bid BID
+	value, _ := strconv.ParseFloat(cotacao.USDBRL.BID, 32)
+	bid.BID = float32(value)
+	log.Println("BID:", bid)
+	json.NewEncoder(w).Encode(bid)
+
+	// select {
+	// case <-time.After(10 * time.Second):
+	// 	w.WriteHeader(http.StatusRequestTimeout)
+	// 	w.Write([]byte("Request com tempo esgotado\n"))
+
+	// case <-ctx.Done():
+	// 	log.Println("Request cancelado pelo cliente")
+	// 	w.WriteHeader(http.StatusBadRequest)
+	// 	// http.Error(w, "Request cancelado pelo cliente", http.StatusRequestTimeout)
+	// }
+}
+
+func getDolarRate(ctx context.Context) (cotacao *USDBRL, err error) {
+	r, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		log.Println("Parece que não achou", err.Error())
+		return
+	}
+
+	rs, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return
+	}
+	defer rs.Body.Close()
+
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &cotacao)
+	return
 }
